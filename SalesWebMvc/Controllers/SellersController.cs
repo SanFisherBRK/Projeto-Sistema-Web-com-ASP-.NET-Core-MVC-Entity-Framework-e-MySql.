ee3b7ef5809b11c4e830e427336a8182bb7a4171
Souza <sanfisher@msn.com>
2022-05-27 20:35:58 -0300

    SellerService and basic FindAll

--- SalesWebMvc/Controllers/SellersController.cs
+++ SalesWebMvc/Controllers/SellersController.cs
@@ -3,14 +3,25 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using SalesWebMvc.Services;
 
 namespace SalesWebMvc.Controllers
 {
     public class SellersController : Controller
     {
-        public IActionResult Index()
+        //Declarando uma dependencia para o SellerService
+        private readonly SellerService _sellerService;
+
+        //Criando o contrutor para injetar a dependencia.
+        public SellersController(SellerService sellerService)
         {
-            return View();
+            _sellerService = sellerService;
+        }
+
+        public IActionResult Index() //Vai chamar nossa operação FindAll() lá da nossa classe SellerService
+        {
+            var list = _sellerService.FindAll();
+            return View(list);
         }
     }
 }
---
+++ SalesWebMvc/Services/SellerService.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using SalesWebMvc.Data;
+using SalesWebMvc.Models;
+
+namespace SalesWebMvc.Services
+{
+    public class SellerService
+    {
+        //Readonly faz com que essa dependencia não seja alterada.
+        private readonly SalesWebMvcContext _context;
+
+        //Criando um construtor para que a dependecia possa ocorrer
+        public SellerService (SalesWebMvcContext context)
+        {
+            _context = context;
+        }
+            
+        public List<Seller> FindAll() 
+        {
+            //Acessa a minha fonte de dados relacionado a tabela vendedores e converte para uma lista.
+            return _context.Seller.ToList();
+        } 
+    }
+}
