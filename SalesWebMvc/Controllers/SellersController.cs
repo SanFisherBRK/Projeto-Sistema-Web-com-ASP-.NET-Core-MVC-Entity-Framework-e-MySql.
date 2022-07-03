3ff344513e099705d8408c510ee59589fe1947a8
Souza <sanfisher@msn.com>
2022-07-02 21:30:40 -0300

    Validation

--- SalesWebMvc/Controllers/SellersController.cs
+++ SalesWebMvc/Controllers/SellersController.cs
@@ -43,6 +43,17 @@ namespace SalesWebMvc.Controllers
         [ValidateAntiForgeryToken]
         public IActionResult Create(Seller seller)
         {
+            //Testando se é valido fica execultando enquanto o usuario não preencher o formulario
+            if (!ModelState.IsValid)
+            {
+                //Carregando os departamentos
+                var departments = _departmentService.FindAll();
+
+                var viewModel = new SellerFormViewModel { Seller = seller, Departments = departments };
+
+                return View(viewModel);
+            }
+
             _sellerService.Insert(seller);
             return RedirectToAction(nameof(Index));
         }
@@ -103,7 +114,18 @@ namespace SalesWebMvc.Controllers
         [ValidateAntiForgeryToken]
         public IActionResult Edit(int id, Seller seller)
         {
-            if(id != seller.Id)
+            //Testando se é valido fica execultando enquanto o usuario não preencher o formulario
+            if (!ModelState.IsValid)
+            {
+                //Carregando os departamentos
+                var departments = _departmentService.FindAll();
+
+                var viewModel = new SellerFormViewModel { Seller = seller, Departments  = departments };
+
+                return View(viewModel);
+            }
+
+            if (id != seller.Id)
             {
                 return RedirectToAction(nameof(Error), new { message = "Id mismatch" }); ;
             }
--- SalesWebMvc/Models/Seller.cs
+++ SalesWebMvc/Models/Seller.cs
@@ -8,17 +8,28 @@ namespace SalesWebMvc.Models
     public class Seller
     {
         public int Id { get; set; }
+
+        //Anotation
+        [Required(ErrorMessage = "{0} required")] //Faz esse campo ser obrigatório
+                                                          //"Name size should be between 3 and 60       //{1} pega o primeiro parametro que é 60     {2} pega o segundo parametro que é 3
+        [StringLength(60, MinimumLength = 3, ErrorMessage = "{0} size should be between {2} and {1}")] //Configura tamanho minimo e maximo do nome \\ErrorMessage personaliza uma mensagem caso o campo esteja vazio
         public string Name { get; set; }
 
+        [Required(ErrorMessage = "{0} required")] //Faz esse campo ser obrigatório
+        [EmailAddress(ErrorMessage = "Enter a valid email")]
         [DataType(DataType.EmailAddress)]
         public string Email { get; set; }
 
 
+        [Required(ErrorMessage = "{0} required")] //Faz esse campo ser obrigatório
         [Display(Name = "Birth Date")]
         [DataType(DataType.Date)]
         [DisplayFormat(DataFormatString = "{0:dd/MM/yyyy}")]
         public DateTime BirthDate { get; set; }
 
+
+        [Required(ErrorMessage = "{0} required")] //Faz esse campo ser obrigatório
+        [Range(1000.0, 50000.0, ErrorMessage = "{0} must be from {1} to {2}")]
         [Display(Name = "Base Salary")]
         [DisplayFormat(DataFormatString = "{0:f2}")] //Coloca duas casas decimais Nosalario do vendedor que aparece na pagina.
         public double BaseSalary { get; set; }
